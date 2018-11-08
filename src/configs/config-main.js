7b95446d7a4917532adc1fbedd54d21b0b2d25d7
Sean Aguinaga <sean.aguinaga@me.com>
2018-11-08 09:01:27 -0800

    Initial commit

---
+++ src/configs/config-main.js
@@ -0,0 +1,8 @@
+/* Main app configs go here */
+
+export const appConfig = {
+  name: 'Pipeline',
+  shortName: 'Pipeline',
+  description: 'ToDo list with repeating ToDos',
+  splashScreenBackground: '#ffffff'
+}
